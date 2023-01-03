a00e0ca140dfc23fd637808767e5a88ac9acdb66
redanaser0@gmail.com <Nram1598562>
2023-01-03 17:40:45 +0200

    l

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -515,7 +515,27 @@ class HomeScreen extends GetView<HomeController> {
               )
             ]);
           }),
+        ),   bottomNavigationBar: Directionality(
+        textDirection: TextDirection.rtl,
+        child: BottomNavigationBar(
+          onTap: (v){},
+
+          showUnselectedLabels: true,showSelectedLabels: true,
+
+          items: [
+            BottomNavigationBarItem(
+              icon:Icon(Icons.home),
+              label: "الرئيسية", ),
+            BottomNavigationBarItem(
+                icon:Icon(Icons.add),
+                label: "التقارير"),
+            BottomNavigationBarItem(
+                icon:Icon(Icons.ac_unit_sharp),
+                label: "الطلبات"),
+
+          ],
         ),
+      ),
       ),
     );
   }
